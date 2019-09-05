61f1724bb88390cf411e026a88890f3ae27693f4
aldajani <boss@Bosss-MacBook-Air.local>
2019-09-04 20:12:15 -0700

    Added all components and fixed styled components issue

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = process.env.PORT || 3004;
+const port = 3004;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+const indexPath = path.join(__dirname, 'public/index.html');
+
+app.get('/businesses/:id', (req, res) => {
+  console.log(indexPath);
+  res.sendFile(indexPath);
+});
+
 app.get('/api/reservations/', (req, res) => {
   console.log(`${req.method} request from ${req.url}`);
   Axios.get('http://localhost:3000/api/reservations/')
     .then((response) => {
+      console.log('inside Axios');
       res.status(200).send(response.data);
     })
     .catch((error) => {
       console.log(error);
-      res.status(200);
+      res.status(404);
     });
 });
 
